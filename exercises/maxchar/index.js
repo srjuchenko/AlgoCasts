0b76db912da87564e3b6c3bf1342b595f443038d
srjuchenko <srjuchenko@gmail.com>
2023-03-01 19:59:13 +0200

    solved the fizzbuzz quastion

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const dict = {};
+  let max = 1;
+  let charMax = str[0];
+
+  for (let i = 0; i < str.length; i++) {
+    dict[str[i]] = dict[str[i]] + 1 || 1;
+    if (dict[str[i]] > max) {
+      max = dict[str[i]];
+      charMax = str[i];
+    }
+  }
+  return charMax;
+}
 
 module.exports = maxChar;
