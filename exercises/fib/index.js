4ba315c641d52210d9cb2d7a87154e7342b0b7df
srjuchenko <srjuchenko@gmail.com>
2023-03-06 11:43:49 +0200

    solved fibo question

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,21 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  if (n === 2) return 1;
+
+  const arr = [1, 1];
+  let currentFib = 3;
+
+  while (currentFib <= n) {
+    let fibN = arr[0] + arr[1];
+    arr[0] = arr[1];
+    arr[1] = fibN;
+    currentFib++;
+  }
+  return arr[1];
+}
 
 module.exports = fib;
